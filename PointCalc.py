325867417aa4b6d76e5c940d0cb8812b6f4334a5
koheikubo_windows <jiubaogengping@gmail.com>
2024-05-11 16:46:06 +0900

    Added PointCalc.py & MVP

---
+++ PointCalc.py
@@ -0,0 +1,66 @@
+class PointCalc:
+    
+    '''
+    Constructor
+    '''
+    def __init__(self, team_setting):
+        self.team_setting = team_setting
+        self.point_board = []
+        self.score_board = []
+        self.next_pointer = 0
+
+    '''
+    Score Manipulation
+    '''
+    def add(self, point):
+        self.point_board.append([self.next_player(), point])
+        score, is_continue = self.update_score()
+        self.next_pointer += 1
+        return score, is_continue
+    
+    def back(self, num):
+        self.point_board = self.point_board[:-num]
+        self.update_score()
+        self.next_pointer -= num
+        return self.next_pointer
+    
+    def update_score(self):
+        score = 0
+        for point in self.point_board:
+            score += point[1]
+            score, is_continue = self.check_score(score)
+            self.score_board.append(score)
+        return score, is_continue
+    
+    def next_player(self):
+        return self.team_setting["team_member"][self.next_pointer % self.team_setting["team_menber_num"]]
+
+    def check_score(self, score):
+        if(score > 50):
+            score = 25
+            return score, True
+        elif(score == 50):
+            return score, False
+        else:
+            return score, True
+    
+    '''
+    Get Function
+    '''
+    def get_team_setting(self):
+        return self.team_setting
+    
+    def get_next_pointer(self):
+        return self.next_pointer
+    
+    def get_point_board(self):
+        return self.point_board
+    
+    def get_score_board(self):
+        return self.score_board
+            
+    def get_score(self):
+        return self.score_board[-1]
+    
+    def get_team_name(self):
+        return self.team_setting["team_name"]
--- main_calculator.py
+++ main_calculator.py
@@ -1,34 +1,64 @@
-# Calculate point (for single team)
-class PointCalc:
-    team_param = {}
+from PointCalc import PointCalc
 
-    def __init__(self, team_param):
-        self.team_param = team_param
+# Create Single Team    
+def create_team(team_name):
+    team_menber = []
+    team_menber_num = int(input(f"Enter {team_name} number of menber : ")) # todo : exeption
+    for i in range(team_menber_num):
+        team_menber.append(input(f"Enter {team_name} menber{i} name : "))
 
-# Create single team    
-def create_team(num, team_menber):
-    team_param = {"team_name" : "team"+str(num),
-                  "team_member" : team_menber
-                 }
-    team = PointCalc(team_param)
+    team_setting = {
+                    "team_name" : team_name,
+                    "team_menber_num" : len(team_menber),
+                    "team_member" : team_menber
+                   }
+    team = PointCalc(team_setting)
     return team
 
-def main():
-    team_num = int(input("Enter number of teams  : "))
+# Team Setting
+def game_setting():
+    team_num = int(input("Enter number of teams  : ")) # todo : exeption
     teams = []
     
     # Create teams
     for i in range(team_num):
-        # Create single team
-        team_menber = []
-        team_menber_num = int(input(f"Enter team{i} number of menber : "))
-        for j in range(team_menber_num):
-            team_menber.append(input(f"Enter team{i} menber{j} name : "))
-            
-        teams.append(create_team(i, team_menber))
+        team_name = "team" + str(i)
+        teams.append(create_team(team_name))
 
     for i, team in enumerate(teams):
-        print(f"team{i}\n  {team.team_param}")
+        print(f"team{i}\n  {team.get_team_setting()}")
+
+    return teams
+
+# Start Game Loop
+def game_start(teams):
+    while(True):
+        is_continue = None
+        for team in teams:
+            print(f"Next Player >> {team.get_team_name()} : {team.next_player()}")
+            score, is_continue = team.add(int(input(f"Enter {team.next_player()} Point : "))) # todo : exeption
+            print(f"{team.get_team_name()} Total Score : < {score} >")
+            print(f"{team.get_team_name()} Point Board: < {team.get_point_board()} >")
+            if(not is_continue):
+                print(f"Game finished! Winner is {team.get_team_name()}")
+                break
+        if(not is_continue):
+            break
+            
+def main():
+    '''
+    Setting
+    '''
+    teams = game_setting()
+    
+    '''
+    Game
+    '''
+    game_start(teams)
+
+    '''
+    Finish, Next game
+    '''
 
     return 0
 
